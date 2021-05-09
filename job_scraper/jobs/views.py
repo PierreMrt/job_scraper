98ea8d974fab1df38f657f98de0f795bda5656e8
liefwerk <natjageni@gmail.com>
2021-05-09 12:09:18 +0200

    added serializers.py & updated views.py for jobs

--- job_scraper/jobs/views.py
+++ job_scraper/jobs/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.db.models import Q
+from django.http import Http404
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+# from .models import Product, Category
+# from .serializers import ProductSerializer, CategorySerializer
