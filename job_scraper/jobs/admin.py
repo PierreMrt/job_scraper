ffa9688364e43afe7dc8be1540cd1ea971215f11
liefwerk <natjageni@gmail.com>
2021-05-09 22:27:29 +0200

    added models in the admin

--- job_scraper/jobs/admin.py
+++ job_scraper/jobs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Links, Search, Results
+
+admin.site.register(Links)
+admin.site.register(Search)
+admin.site.register(Results)
