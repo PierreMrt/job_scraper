49b93fa90ea4ba0defa9385a1d6ad37d294b3dee
PierreMrt <merlet.pierre@outlook.com>
2021-05-10 19:23:49 +0200

    updated models.py

--- job_scraper/jobs/models.py
+++ job_scraper/jobs/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 # importation de user depuis Django
 from django.contrib.auth.models import User
+from django.utils import timezone
+from jobs.libs.scraping import LinkedIn, Monster, Indeed
+from datetime import datetime
+
 
 
 
@@ -20,7 +24,22 @@ class Links(models.Model):
     linkedIn = models.CharField(max_length=255)
     monster = models.CharField(max_length=255)
     indeed = models.CharField(max_length=255)
-# C'est à cet endroit que l'on ajoute les fonctions pour afficher les infos dans l'API.
+
+    def fetch(self, country):
+        links = Links.objects.filter(country=country)[0]
+        return (links.extension, links.monster, links.indeed)
+
+    @staticmethod
+    def populate():
+        links = [('italy', 'it', 'www.linkedin.com', 'https://www.monster.it/lavoro/cerca?', 'https://it.indeed.com/offerta-lavoro?'),
+            ('france', 'fr', 'www.linkedin.com', 'https://www.monster.fr/emploi/recherche?', 'https://fr.indeed.com/voir-emploi?'),
+            ('uruguay', 'uy', 'www.linkedin.com', 'None', 'https://uy.indeed.com/descripción-del-puesto?'),
+            ('austria', 'at', 'www.linkedin.com', 'https://www.monster.at/jobs/suche?', 'https://at.indeed.com/Zeige-Job?')]
+        
+        for r in links:
+            l = Links(country=r[0], extension=r[1], linkedIn=r[2], monster=r[3], indeed=r[4])
+            l.save()
+
 
 class Search(models.Model):
     ## j'ai importé User des modèles pré-fabriqués de Django
@@ -39,6 +58,7 @@ class Search(models.Model):
         if self.search_key not in actives:
             self.save()
             print('added to db')
+            Results().scrap(self.job, self.country)
         else:
             print(f'Search for {self.job} in {self.country} is already active.')
         
@@ -49,7 +69,7 @@ class Search(models.Model):
 
 
 class Results(models.Model):
-    search_key = models.ForeignKey(Search, on_delete=models.CASCADE)
+    search_key = models.CharField(max_length=255)
     source = models.CharField(max_length=255)
     job_id = models.CharField(max_length=255)
     job_title = models.CharField(max_length=255)
@@ -60,3 +80,29 @@ class Results(models.Model):
     date = models.DateTimeField()
     link = models.CharField(max_length=255)
 
+    def add_results(self, r):
+        new_entry = Results(search_key=r['search_key'], source=r['source'], job_id=r['job_id'], job_title=r['job_title'], 
+                            description=r['description'], company=r['company'], location=r['location'],
+                            country=r['country'], date=r['date'], link=r['link'])
+        new_entry.save()
+
+    def scrap(self, job, country):
+        links = Links().fetch(country)
+        cache = self.cached_ids()
+
+        linkedin = LinkedIn(cache, job, country)
+        for r in linkedin.results:
+            self.add_results(r)
+
+        # Indeed(links, cache, job, country)
+        # Monster(links, cache, job, country)
+
+    def cached_ids(self):
+        cached_ids = set()
+        results = Results.objects.all()
+        [cached_ids.add(r.job_id) for r in results]
+        return cached_ids
+
+def main():
+    s = Search(user='pierre', job='data_analyst', country='france')
+    s.new_search()
