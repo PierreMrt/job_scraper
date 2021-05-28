f78a694279db0542875755f03b3b95b8b9259307
PierreMrt <merlet.pierre@outlook.com>
2021-05-28 14:23:37 +0200

    Cleaned urls

--- django_job_search/jobs/urls.py
+++ django_job_search/jobs/urls.py
@@ -8,9 +8,5 @@ urlpatterns = [
     path('', SearchView.as_view(), name='home'),
     path('add/', SearchCreateView.as_view()),
     path('update', views.update_search, name='update'),
-    # path('results/<str:search_key>/', views.create_form, name='results'),
-    # path('results/<str:search_key>/', views.show_results, name='results'),
-    # path('results/<str:search_key>/', ResultView.as_view(), name='results'),
-    # path('results/<str:search_key>/', ResultView.as_view(), name='results'),
     path(f'results/<str:search_key>/', ResultView.as_view(), name='filtered_results'),
 ]
